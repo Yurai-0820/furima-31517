e0cae634104ad80e68b92c128de08ee23a273678
Yurai-0820 <marin3150820@gmail.com>
2021-02-12 23:03:02 +0900

    users関係修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :password
     validates :birthday
 
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥]/, message: 'is invalid. input full-width characters' } do
