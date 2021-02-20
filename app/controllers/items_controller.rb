9d7d77617c832cf594003edf11a74df525a8821d
Yurai-0820 <marin3150820@gmail.com>
2021-02-21 00:26:03 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,16 +23,17 @@ class ItemsController < ApplicationController
   def show
   end
 
-  # def edit
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
 
-  # def update
-  #   if @item.update(item_params)
-  #     redirect_to item_path(@item)
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
 
   # def destroy
   #   if @item.destroy
