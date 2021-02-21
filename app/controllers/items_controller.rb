3f8a76fb51ee4034ee628381195387aa3b9b94fc
Yurai-0820 <marin3150820@gmail.com>
2021-02-21 20:16:02 +0900

    条件式追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :items, only: [:edit, :update, :show, :destroy]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -54,6 +55,10 @@ class ItemsController < ApplicationController
   end
 def items
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
